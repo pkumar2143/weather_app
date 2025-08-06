9fbfd338b50689ae62e7ab317ea5aeffcc380b53
Pro Kumar <pkumar@udel.edu>
2025-08-06 16:45:58 -0400

    getter methods (testing) for LocationHunter

--- weatherAgents.py
+++ weatherAgents.py
@@ -12,7 +12,34 @@ class LocationHunter(DataHunter):
         self._latitude    = self._location.latitude
         self._longitude   = self._location.longitude
 
-    ## Create getter-setter methods 
+    ## Create getter methods    
+    @property
+    def location(self):
+        '''
+        Returns full location information as in the NWS DB.
+        Some info not available all the time (e.g. some locations have zipcodes, others do not.)
+        Dtype str
+        '''
+        return self._location
+    
+    @property
+    def latitude(self):
+        '''
+        Returns latitude of the user-desired location.
+        Dtype float
+        '''
+        return self._latitude
+    
+    @property
+    def longitude(self):
+        '''
+        Returns longitude of the user-desired location.
+        Dtype float
+        '''
+        return self._longitude
+    
+    # Setter methods may not make sense, since they should only change if we change locations.
+
 
 class PropertiesHunter(DataHunter):
     def __init__(self, textLocation):
--- weatherAgents_test.py
+++ weatherAgents_test.py
@@ -5,6 +5,13 @@ list_of_locations = ["Philadelphia, PA", "Hanover, MD", "Columbus, OH", "West Po
 for location in list_of_locations:
     print(f"\n ----- {location} ----- ")
     hunter1 = LocationHunter(location)
-    print(hunter1._location)
-    print(hunter1._latitude)
-    print(hunter1._longitude)
+
+    print(hunter1.geolocator)
+    print(hunter1.location)
+    print(hunter1.latitude)
+    print(hunter1.longitude)
+
+    # Should not be accessed in such a manner
+    #print(hunter1._location)
+    #print(hunter1._latitude)
+    #print(hunter1._longitude)
